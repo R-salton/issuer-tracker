8760883051cf5674633779fb590ec3abcf556283
R-salton <ringusa22@gmail.com>
2025-07-09 14:14:06 +0200

    Configure Google Provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,13 @@
-import NextAuth from "next-auth"
 
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
   providers: [
     // Add your providers here
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
   ],
 })
 
